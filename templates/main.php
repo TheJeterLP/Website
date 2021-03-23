1ab750f232a64b227e85f3928e19c5c36f5706aa
Joey Peter <joey.peter1998@gmail.com>
2021-03-23 15:57:55 +0100

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <h1 class="title is-size-1-desktop is-size-2-touch is-uppercase is-family-code glitch" data-text="TheJeterLP">
     TheJeterLP
 </h1>
@@ -5,3 +7,5 @@
     <span class="icon is-padd"><i class="fa fa-angle-double-right"></i></span>
     The next generation developer
 </h2>
+<?php
+?>
