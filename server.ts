5bdda57f0d1b3717746b98778e135be0e82829b4
TheJeterLP <joey.peter1998@gmail.com>
2022-12-30 17:33:16 +0100

    add comments back

--- server.ts
+++ server.ts
@@ -1,23 +1,48 @@
+/**
+ * Required external modules
+ */
 import express, { Application } from 'express';
 import path from 'path';
+
+/**
+ * Required internal modules
+ */
 import { info } from './logmanager';
 import { DBManager } from './dbmanager';
 import { loadRoutes } from './routemanager';
 
+/**
+ * Required configuration sections
+ */
 import { website_port } from './config.json';
 
+/**
+ * App Variables
+ */
 const app: Application = express();
-const db: DBManager = new DBManager();
 
+/**
+ * SQL Connection.
+ */
+const db: DBManager = new DBManager();
 db.createTables();
 
+/**
+ * App Configuration
+ */
 app.use(express.json());
 app.set('views', path.join(path.join(__dirname, 'views'), 'frontend'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Routes Definitions
+ */
 loadRoutes(app, db);
 
+/**
+ * Server Activation
+ */
 app.listen(website_port, () => {
     info(`Listening to requests at 127.0.0.1:${website_port}`);
 });
